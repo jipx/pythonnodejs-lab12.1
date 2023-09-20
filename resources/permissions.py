2e99b1d9c0c4f30872718747cf5f62f292275fce
EC2 Default User <ec2-user@ip-10-16-10-125.ec2.internal>
2023-09-20 07:12:04 +0000

    add cognito login to the web site

--- resources/permissions.py
+++ resources/permissions.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 S3API = boto3.client("s3", region_name="us-east-1") 
-bucket_name = "<FMI>"
+bucket_name = "c81790a1735464l4721648t1w414728139848-s3bucket-1i2a5ied6a6ad"
 
 policy_file = open("/home/ec2-user/environment/resources/public_policy.json", "r")
 
--- resources/website/config.js
+++ resources/website/config.js
@@ -1,4 +1,4 @@
 window.COFFEE_CONFIG = {
-	API_GW_BASE_URL_STR: null,
-	COGNITO_LOGIN_BASE_URL_STR: null
+	API_GW_BASE_URL_STR: "https://n4juxly2m5.execute-api.us-east-1.amazonaws.com/prod",
+	COGNITO_LOGIN_BASE_URL_STR: "https://jipx-2023-09-20.auth.us-east-1.amazoncognito.com/login?client_id=516v2oqsqli3ja1cuutmdu4oc&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fd3ah0id3u91x47.cloudfront.net%2Fcallback.html"
 };
